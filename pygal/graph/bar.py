596cfce48dd7f81d2ebc3249a7c10ba46567ba7d
tshore2004 <63600678+tshore2004@users.noreply.github.com>
2021-04-28 21:38:13 -0400

    Update bar.py

--- pygal/graph/bar.py
+++ pygal/graph/bar.py
@@ -84,8 +84,8 @@ class Bar(Graph):
 
         if self.print_values_position == 'top':
             if self.horizontal:
-                x = (x_bottom + sign * self.style.value_font_size / 2)+500
-                y = y_center
+                x = (x_bottom + sign * self.style.value_font_size / 2)
+                y = y_center + 500
             else:
                 x = x_center
                 y = y_bottom - sign * self.style.value_font_size / 2
